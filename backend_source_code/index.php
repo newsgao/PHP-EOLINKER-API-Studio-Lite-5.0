9533e0ee4ce9b65953646ee87684f3df6d8cb224
EOLINKER R&D Supporter <dev@eolinker.com>
2018-12-18 10:12:43 +0800

    Update index.php

--- backend_source_code/index.php
+++ backend_source_code/index.php
@@ -1,22 +1,25 @@
 <?php
 /**
- * @name eolinker open source，eolinker开源版本
+ * @name eolinker open source，EOLINKER OPEN SOURCE
  * @link https://www.eolinker.com
  * @package eolinker
- * @author www.eolinker.com 广州银云信息科技有限公司 2015-2018
+ * @author www.eolinker.com EOLINKER co,.LTD 2015-2018
 
- * eolinker，业内领先的Api接口管理及测试平台，为您提供最专业便捷的在线接口管理、测试、维护以及各类性能测试方案，帮助您高效开发、安全协作。
- * 如在使用的过程中有任何问题，可通过http://help.eolinker.com寻求帮助
+ * ELOINKER, industry-leading Api management and test platform provides you with the most professional and convenient online interface management, testing, maintenance and various performance testing solutions to help you develop efficiently and securely.
+ * If you have any problems during the process of use
  *
- * 注意！eolinker开源版本遵循GPL V3开源协议，仅供用户下载试用，禁止“一切公开使用于商业用途”或者“以eoLinker开源版本为基础而开发的二次版本”在互联网上流通。
- * 注意！一经发现，我们将立刻启用法律程序进行维权。
- * 再次感谢您的使用，希望我们能够共同维护国内的互联网开源文明和正常商业秩序。
+ * 
+ * Note! The eolinker open source version follows the GPL V3 open source protocol and is only available for users to download and try.  
+ * It is forbidden to "all publicly available for commercial use" or "a second version developed on the basis of the open source version 
+ * of EOLINKER" is circulated on the Internet.
+ * Attention! Once discovered, we will immediately activate legal procedures for rights protection.
+ * Thank you again for your use, I hope we can jointly maintain the domestic Internet open source civilization and normal business order.
  *
  */
 
-//定义框架路径
+//define framework
 defined('PATH_FW') or define('PATH_FW', './RTP');
 
-//引入框架
+//require path directory
 require PATH_FW . DIRECTORY_SEPARATOR . 'rtp.inc.php';
 ?>
