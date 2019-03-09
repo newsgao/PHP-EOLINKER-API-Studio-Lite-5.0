6007d56d7f4bf7fab43588212b43324965a96583
Lijing Ye <yelijing18@gmail.com>
2019-03-09 23:01:37 +0800

    fix(frontend-end): 修复无法保存接口
    后端未返回必须的字段，没有做兼容导致无法保存（本修复是不必要的，只要后端返回了这三个字段就好了）

--- frontend_source_code/src/app/ui/content/home/project/inside/content/api/edit/index.js
+++ frontend_source_code/src/app/ui/content/home/project/inside/content/api/edit/index.js
@@ -1273,9 +1273,10 @@
                                 vm.interaction.response.apiInfo.apiProtocol = "" + vm.interaction.response.apiInfo.apiProtocol;
                                 vm.interaction.response.apiInfo.apiRequestType = "" + vm.interaction.response.apiInfo.apiRequestType;
                                 vm.interaction.response.apiInfo.apiRequestParamType = "" + vm.interaction.response.apiInfo.apiRequestParamType;
-                                vm.interaction.response.apiInfo.apiNoteType = "" + vm.interaction.response.apiInfo.apiNoteType;
-                                vm.interaction.response.apiInfo.apiRichNote = vm.interaction.response.apiInfo.apiNoteType == '0' ? vm.interaction.response.apiInfo.apiNote : '';
-                                vm.interaction.response.apiInfo.apiMarkdownNote = vm.interaction.response.apiInfo.apiNoteType == '1' ? vm.interaction.response.apiInfo.apiNote : '';
+                                vm.interaction.response.apiInfo.apiNoteType = "" + fun.getOrDefault(vm.interaction.response.apiInfo.apiNoteType, '1');
+                                vm.interaction.response.apiInfo.apiNoteRaw = fun.getOrDefault(vm.interaction.response.apiInfo.apiNoteRaw, '');
+                                vm.interaction.response.apiInfo.apiRichNote = vm.interaction.response.apiInfo.apiNoteType == '0' ? fun.getOrDefault(vm.interaction.response.apiInfo.apiNote, '') : '';
+                                vm.interaction.response.apiInfo.apiMarkdownNote = vm.interaction.response.apiInfo.apiNoteType == '1' ? fun.getOrDefault(vm.interaction.response.apiInfo.apiNote, '') : '';
                                 try {
                                     response.apiInfo.authInfo.status = response.apiInfo.authInfo.status || "0";
                                 } catch (e) {}
@@ -1375,6 +1376,12 @@
             } else {
                 fun.initCommonExpression();
             }
+        }
+        fun.getOrDefault = function (optional, defaultValue) {
+            if (optional == undefined) {
+                return defaultValue;
+            }
+            return optional;
         }
         vm.fun.init = function () {
             if (GroupService.get()) {
